eb21753b03ad19c454636f0aa69ed2e8f252f867
ranjan_4590 <ranjangupta35558@gmail.com>
2022-11-12 12:03:13 +0530

    index adding-gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import hero from '../assets/hero.png';
 const Hero = () => {
   return (
-    <div>
-        <div></div>
+    <div className='relative h-auto w-auto flex flex-col'>
+        <div className=''></div>
+        <div className=''>
+        <div className=''>
+        <h1 className=''>Play With Your Electric Feet</h1>
+        <h1 className=''>Adapt 2.0 generation Sneekers</h1>
+        <button className=''> Explore</button>
+        {/* <div>video</div>
+        <div>social</div> */}
+        </div>
+        
         <div>
-        <div></div>
-        <div></div>
+          <img src={hero} alt='hero-img/img' className=''/>
+        </div>
         </div>
         <div></div>
         <div></div>
