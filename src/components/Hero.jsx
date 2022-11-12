3ae608aba359270326f2f65379b3f8dbc3c88eda
ranjan_4590 <ranjangupta35558@gmail.com>
2022-11-12 11:35:06 +0530

    adding image

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+        <div></div>
+        <div></div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Hero
