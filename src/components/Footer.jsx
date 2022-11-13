e520af736c5eaf7e4c201ce3fc35efa290ae3563
ranjan_4590 <ranjangupta35558@gmail.com>
2022-11-13 16:48:29 +0530

    footerAPI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import footerAPI from "../data/footer";
 
-const Footer = () => {
+const Footer = ({ footerAPI: { title, links } }) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      {" "}
+      <div>
+        <div>
+          {title.map((val, i) => (
+            <div key={i} className="">
+              <h1>{val.title}</h1>
+            </div>
+          ))}
+          {links.map((list, i) => (
+            <ul key={i}>
+              {list.map((link, i) => (
+                <li key={i} className="">
+                  {link.link}
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
