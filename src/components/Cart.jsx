a1cc8b44181374afcf719dde29a3985870356115
ranjan_4590 <ranjangupta35558@gmail.com>
2022-11-14 19:02:14 +0530

    adding bag

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,7 +7,7 @@ const Cart = () => {
   return (
     <div>
       <div
-        className={`fixex top-0 left-0 right-0 bottom-0 blur-effect-theme duration-500  w-full h-screen opacity-100 z-[250]`}
+        className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme  w-full h-screen opacity-100 z-[250]`}
       >
         <div
           className={`blur-effect-theme duration-500 h-screen max-w-xl w-full absolute right-0`}
--- src/components/cart/CartEmpty.js
+++ src/components/cart/CartEmpty.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import bag from "../assets/blank-paper.png";
 const CartEmpty = () => {
   return (
-    <div>CartEmpty</div>
+    <div>
+       <div>
+          <div>
+               <img src={bag} alt='img/empty-bag' className='h-50 w-50'/>
+          </div>
+       </div>
+    </div>
   )
 }
 
-export default CartEmpty
+export default CartEmpty;
