0ad28a297be04f879d0d772378fee5caa8bf7228
ranjan_4590 <ranjangupta35558@gmail.com>
2022-11-16 19:02:58 +0530

    cartitems

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import Store from './Store/Store.js';
+// import { Toaster } from 'react-hot-toast';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={Store} >
+  {/* <Toaster position='top-center' reverseOrder={false} /> */}
     <App />
   </Provider>
   </React.StrictMode>
